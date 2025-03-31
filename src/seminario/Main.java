bfd79824f00127531d196318b6daf9c7eb5ad64e
Cristian Fernández <cristianfernandez@gmail.com>
2025-03-30 23:36:53 -0300

    oo

--- src/seminario/Main.java
+++ src/seminario/Main.java
@@ -5,8 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import seminario.view.SplashScreen;
 
 public class Main extends Application {
+
+    @Override
+    public void init() throws Exception {
+        // Esto se ejecuta ANTES del start(), en segundo plano
+        // Inicializa la DB
+        // Verifica sesión activa
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -17,7 +25,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("App Prestamo");
+        System.setProperty("javafx.preloader", SplashScreen.class.getName());
         launch(args);
     }
 }
---
+++ src/seminario/model/Cliente.java
@@ -0,0 +1,5 @@
+package seminario.model;
+
+public class Cliente {
+    private Long nroDni;
+}
